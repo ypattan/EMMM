0ceb387a7df6400ce2ccf70202289b804056c05f
Yamini Pattan <yamini@Yaminis-MacBook-Pro.local>
2020-08-23 02:44:52 -0700

    working 'try again' button that goes to cleared MainActivity

--- app/src/main/java/com/example/decisionpicker/AnswerActivity.kt
+++ app/src/main/java/com/example/decisionpicker/AnswerActivity.kt
@@ -1,7 +1,9 @@
 package com.example.decisionpicker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class AnswerActivity : AppCompatActivity() {
@@ -10,10 +12,19 @@ class AnswerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_answer)
 
         setAnswer()
+
+        findViewById<Button>(R.id.again_button).setOnClickListener{
+            goTryAgain()
+        }
     }
 
     private fun setAnswer() {
         val ans = findViewById<TextView>(R.id.answer)
         ans.text = intent.getStringExtra("Answer")
+    }
+
+    private fun goTryAgain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
