69544d5b1006723974658a55622f4c70993d822b
Yamini Pattan <yamini@Yaminis-MacBook-Pro.local>
2020-08-23 02:36:28 -0700

    goes to next activity and displays answer

--- app/src/main/java/com/example/decisionpicker/AnswerActivity.kt
+++ app/src/main/java/com/example/decisionpicker/AnswerActivity.kt
@@ -2,10 +2,18 @@ package com.example.decisionpicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class AnswerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_answer)
+
+        setAnswer()
+    }
+
+    private fun setAnswer() {
+        val ans = findViewById<TextView>(R.id.answer)
+        ans.text = intent.getStringExtra("Answer")
     }
 }
